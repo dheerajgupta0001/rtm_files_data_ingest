e43c3b5608eee0f92a7855407f9b6c1c19e96a9e
Dheeraj Gupta <dheerajgupta0001.dg@gmail.com>
2021-02-11 01:28:27 +0530

    states horly data push completed

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 from src.config.appConfig import initConfig
 from src.DataFetcher.statesHourlyDataFetcher import getStatesHourlyData
-from src.config.appConfig import getFileMappings
+from src.config.appConfig import getFileMappings, getJsonConfig
 import datetime as dt
+from src.typeDefs.measRecord import IMetricsDataRecord
+from typing import List
 
 initConfig()
 fileObjs = getFileMappings()
-getStatesHourlyData(fileObjs[0], dt.datetime(2021,1,1) )
+stateHourlyRecords:List[IMetricsDataRecord] = getStatesHourlyData(fileObjs[0], dt.datetime(2021,1,1) )
+
 
