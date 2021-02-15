941e144c1ef2b470716ac3692fe3600554efdd19
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2021-02-15 11:57:54 +0530

    freq data import done

---
+++ src/app/freqDailyService.py
@@ -0,0 +1,19 @@
+from src.config.appConfig import getFileMappings, getFreqVoltConfigs, getJsonConfig, getFreqVoltConfigs
+from src.dataFetchers.dataFetcherHandler import getFreqData
+from src.typeDefs.stateConfig import IStateConfig
+from src.repos.measData.measDataRepo import MeasDataRepo
+from typing import List
+import datetime as dt
+
+
+def freqDailyService(freqFilePath):
+    freqVoltConfigs = getFreqVoltConfigs()
+    freqRecords = getFreqData(freqVoltConfigs, freqFilePath)
+    measDataRepo = MeasDataRepo(getJsonConfig()['appDbConnStr'])
+
+    isRawCreationSuccess = measDataRepo.insertDaywiseFreqMetrics(freqRecords)
+
+    if isRawCreationSuccess:
+        print("Freq Daily data insertion SUCCESSFUL")
+    else:
+        print("Freq Daily data insertion UNSUCCESSFUL")
---
+++ src/typeDefs/freqVoltConfig.py
@@ -0,0 +1,6 @@
+from typing import TypedDict
+
+
+class IFreqVoltConfig(TypedDict):
+    data_type: str
+    sheet: str
