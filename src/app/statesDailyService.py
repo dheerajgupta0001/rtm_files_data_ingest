ca7bec97bd0a28bfc6872c41d4e3a02f2c758232
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2021-02-15 13:16:26 +0530

    removed junk

--- src/app/statesDailyService.py
+++ src/app/statesDailyService.py
@@ -1,27 +1,25 @@
 from src.config.appConfig import initConfigs
-from src.config.appConfig import getFileMappings, getJsonConfig , getStateConfigs
+from src.config.appConfig import getFileMappings, getJsonConfig, getStateConfigs
 from src.dataFetchers.dataFetcherHandler import statesDailyDataFetcherHandler
 from src.typeDefs.stateConfig import IStateConfig
 from src.repos.measData.measDataRepo import MeasDataRepo
 from typing import List
 
-initConfigs()
-filesSheet = getFileMappings()
-stateConfigSheet = getStateConfigs()
 
-def statesDailyService(stateConfigSheet :List[IStateConfig], excelFilePath):
-    stateDailyRecords = statesDailyDataFetcherHandler(stateConfigSheet, excelFilePath)
+def statesDailyService(stateConfigSheet: List[IStateConfig], excelFilePath):
+    stateDailyRecords = statesDailyDataFetcherHandler(
+        stateConfigSheet, excelFilePath)
     measDataRepo = MeasDataRepo(getJsonConfig()['appDbConnStr'])
 
     for each in stateDailyRecords:
         isRawCreationSuccess = False
         # print(each)
-        
+
         isRawCreationSuccess = measDataRepo.insertStatesDailyData(each)
 
         if isRawCreationSuccess:
-            print("State Daily data insertion SUCCESSFUL for {0}".format(each[0]['entity_tag']))
+            print("State Daily data insertion SUCCESSFUL for {0}".format(
+                each[0]['entity_tag']))
         else:
-            print("State Daily data insertion UNSUCCESSFUL for {0}".format(each[0]['entity_tag']))
-
-
+            print("State Daily data insertion UNSUCCESSFUL for {0}".format(
+                each[0]['entity_tag']))
--- src/app/statesHourlyService.py
+++ src/app/statesHourlyService.py
@@ -1,27 +1,25 @@
 from src.config.appConfig import initConfigs
-from src.config.appConfig import getFileMappings, getJsonConfig , getStateConfigs
+from src.config.appConfig import getFileMappings, getJsonConfig, getStateConfigs
 from src.dataFetchers.dataFetcherHandler import statesHourlyDataFetcherHandler
 from src.typeDefs.stateConfig import IStateConfig
 from src.repos.measData.measDataRepo import MeasDataRepo
 from typing import List
 
-initConfigs()
-filesSheet = getFileMappings()
-stateConfigSheet = getStateConfigs()
 
-def statesHourlyService(stateConfigSheet :List[IStateConfig], excelFilePath):
-    stateHourlyRecords = statesHourlyDataFetcherHandler(stateConfigSheet, excelFilePath)
+def statesHourlyService(stateConfigSheet: List[IStateConfig], excelFilePath):
+    stateHourlyRecords = statesHourlyDataFetcherHandler(
+        stateConfigSheet, excelFilePath)
     measDataRepo = MeasDataRepo(getJsonConfig()['appDbConnStr'])
 
     for each in stateHourlyRecords:
         isRawCreationSuccess = False
         # print(each)
-        
+
         isRawCreationSuccess = measDataRepo.insertStatesHorlyData(each)
 
         if isRawCreationSuccess:
-            print("State Hourly data insertion SUCCESSFUL for {0}".format(each[0]['entity_tag']))
+            print("State Hourly data insertion SUCCESSFUL for {0}".format(
+                each[0]['entity_tag']))
         else:
-            print("State Hourly data insertion UNSUCCESSFUL for {0}".format(each[0]['entity_tag']))
-
-
+            print("State Hourly data insertion UNSUCCESSFUL for {0}".format(
+                each[0]['entity_tag']))
